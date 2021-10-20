c80822f7d1f7d612236efd464f0539fa02e34627
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-10-19 22:05:02 -0700

    Virtual fixed

--- SuperClass/SuperClass/SuperClass.cpp
+++ SuperClass/SuperClass/SuperClass.cpp
@@ -6,35 +6,63 @@ using namespace std;
 
 class Shape
 {
-    
-public:
-    string color;
-    void getArea(){
-        cout << "area \n";
-    }
+    public:
+        string color;
+        virtual double getArea() = 0;
 };
 
 class Rectangle : public Shape {
 public:
+    Rectangle(double a, double b) {
+        height = a;
+        width = b;
+    }
+
+
+    double getArea(){
+        return height * width;
+    }
+
     double height;
     double width;
 };
 
 class Triangle : public Shape {
 public:
-    double Base;
+    Triangle(double base, double height) {
+        this->base = base;
+        this->height = height;
+    }
+
+    double getArea() {
+        return (base * height) / 2;
+    }
+    double base;
     double height;
 };
 
-class circle : public Shape
+class Circle : public Shape
 {
+public:
+
+    Circle(double r) {
+        radius = r;
+    }
+    double getArea() {
+        return radius * radius * 3.14;
+    }
     double radius;
 };
+
 int main()
 {
-    Rectangle rectangle;
-    rectangle.getArea();
-   
+    Rectangle rectangle(10,20);
+    Circle circle(4.2);
+    Triangle t(10, 20);
+    cout << rectangle.getArea() << endl;
+    cout << circle.getArea() << endl;
+    cout << t.getArea() << endl;
+
     return 0;
     
 }
